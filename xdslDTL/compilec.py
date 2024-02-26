ced16cc234138ddf17fc115c743d6be36add8d4a
edward <edward@edstow.co.uk>
2024-02-26 18:33:57 +0000

    Fiddle with getting xdsl to talk nicely to mlir

--- xdslDTL/compilec.py
+++ xdslDTL/compilec.py
@@ -33,23 +33,33 @@ def compile(module: builtin.ModuleOp, lib_output: str, header_out=None):
     printer.print(module)
     print(res.getvalue())
 
+    fd, path = tempfile.mkstemp()
+    print(f"Making tmp mlir - IR file: {path}")
+    with os.fdopen(fd, 'wb') as tmp:
+        tmp.write(res.getvalue().encode('utf8'))
+
+
 
     print("mlir-opt:")
-    passes = ["--convert-scf-to-cf",
-              "--convert-cf-to-llvm",
-              "--convert-func-to-llvm",
-              "--convert-arith-to-llvm",
-              "--expand-strided-metadata",
-              "--normalize-memrefs",
-              "--memref-expand",
-              "--fold-memref-alias-ops",
-              "--finalize-memref-to-llvm",
-              "--reconcile-unrealized-casts"]
+    passes = [
+        "--convert-scf-to-cf",
+        "--convert-cf-to-llvm",
+        "--convert-func-to-llvm",
+        "--convert-arith-to-llvm",
+        "--expand-strided-metadata",
+        "--normalize-memrefs",
+        "--memref-expand",
+        "--fold-memref-alias-ops",
+        "--finalize-memref-to-llvm",
+        "--reconcile-unrealized-casts",
+    ]
 
     process_opt = subprocess.Popen(['mlir-opt'] + passes, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = process_opt.communicate(res.getvalue().encode('utf8'))
     process_opt.wait()
     print(out)
+    print("ERRS:")
+    print(err)
 
     print("mlir-translate")
     process_translate = subprocess.Popen(['mlir-translate', '--mlir-to-llvmir'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
