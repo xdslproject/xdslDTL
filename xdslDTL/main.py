8ad1747177e49514c98181b4e3eb3b36f3e69079
edward <edward@edstow.co.uk>
2024-08-07 12:07:47 +0100

    refactor compilec into DTL repo from xdslDTL repo. Make xdslDTl repo unnecessary

--- xdslDTL/main.py
+++ xdslDTL/main.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3
-
-import argparse
-
-from xdsl.dialects.experimental.dlt import DLT
-from xdsl.dialects.experimental.dtl import DTL
-from xdsl.xdsl_opt_main import xDSLOptMain
-
-
-class OptMain(xDSLOptMain):
-
-    def register_all_dialects(self):
-        super().register_all_dialects()
-        self.ctx.load_dialect(DTL)
-        self.ctx.load_dialect(DLT)
-
-    def register_all_passes(self):
-        super().register_all_passes()
-        
-    def register_all_targets(self):
-        super().register_all_targets()
-
-
-    def register_all_arguments(self, arg_parser: argparse.ArgumentParser):
-        super().register_all_arguments(arg_parser)
-
-
-def __main__():
-    xdsl_main = OptMain()
-    xdsl_main.run()
-
-if __name__ == "__main__":
-    __main__()
+# #!/usr/bin/env python3
+#
+# import argparse
+#
+# from xdsl.dialects.experimental.dlt import DLT
+# from xdsl.dialects.experimental.dtl import DTL
+# from xdsl.xdsl_opt_main import xDSLOptMain
+#
+#
+# class OptMain(xDSLOptMain):
+#
+#     def register_all_dialects(self):
+#         super().register_all_dialects()
+#         self.ctx.load_dialect(DTL)
+#         self.ctx.load_dialect(DLT)
+#
+#     def register_all_passes(self):
+#         super().register_all_passes()
+#
+#     def register_all_targets(self):
+#         super().register_all_targets()
+#
+#
+#     def register_all_arguments(self, arg_parser: argparse.ArgumentParser):
+#         super().register_all_arguments(arg_parser)
+#
+#
+# def __main__():
+#     xdsl_main = OptMain()
+#     xdsl_main.run()
+#
+# if __name__ == "__main__":
+#     __main__()
